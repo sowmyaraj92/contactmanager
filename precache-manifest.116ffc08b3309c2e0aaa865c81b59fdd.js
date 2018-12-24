5b529f82c9c90ab9885d19f2f6c74439ac6355e0
Sowmya Rajan <LPMB-Bang-Sowmya@FusionChartss-MacBook-Pro-3.local>
2018-12-24 15:43:35 +0530

    Updates

---
+++ precache-manifest.116ffc08b3309c2e0aaa865c81b59fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "fca0534d0e8e566202d9",
+    "url": "/contactmanager/static/js/main.fca0534d.chunk.js"
+  },
+  {
+    "revision": "b7f152b5dfec590aa6e5",
+    "url": "/contactmanager/static/js/1.b7f152b5.chunk.js"
+  },
+  {
+    "revision": "b7f152b5dfec590aa6e5",
+    "url": "/contactmanager/static/css/1.11c0ef70.chunk.css"
+  },
+  {
+    "revision": "a2294f510b42dce7927ea527bbcb5368",
+    "url": "/contactmanager/index.html"
+  }
+];
